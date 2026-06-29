f27f0acb5af03340d5c873902c988e859cf329e4
kbrinkers <kbrinkers@brinkersconsulting.nl>
2026-06-29 16:26:30 +0200

    Refactor next.config.js to an empty configuration

--- next.config.js
+++ next.config.js
@@ -1,1 +1,3 @@
-/** @type {import('next').NextConfig} */const nextConfig = {  reactStrictMode: true,};module.exports = nextConfig;
+/** @type {import("next").NextConfig} */
+const nextConfig = {};
+module.exports = nextConfig;
